929d07d0b3b0105635ad04ef925859fffb62e036
gydud25416 <152379377+gydud25416@users.noreply.github.com>
2024-04-29 19:14:10 +0900

    [Study] Home_headline 추가
    [Study] Home_headline 추가

--- contents_board/src/component/ContentsList.js
+++ contents_board/src/component/ContentsList.js
@@ -3,8 +3,25 @@ import './ContentsList.css';
  
 import { Link } from 'react-router-dom';
 
-const ContentsList =({props, title})=>{
-    
+const ContentsList =({props, title, headline})=>{
+    const headline0 = props.filter((it)=>(it.headline === true));
+    if(headline){
+        return(
+            <>
+            <h2>{title}</h2>
+            <ul className="contentsList">
+                {headline0.map((it)=>(
+                    <li key={it.id}>
+                        <Link to={`/${it.menu}/${it.id}`} props={props}>
+                            <h3>{it.title} </h3>
+                            <p>{it.subTitle}</p>
+                        </Link> 
+                    </li>
+                ))} 
+            </ul>
+            </>
+        )
+    }else{
     return(
         <>
         <h2>{title}</h2>
@@ -20,6 +37,7 @@ const ContentsList =({props, title})=>{
         </ul>
         </>
     )
+}
 }
 
 export default ContentsList;
--- contents_board/src/component/Home.js
+++ contents_board/src/component/Home.js
@@ -1,8 +1,21 @@
+import ContentsList from "./ContentsList";
+import dummy from "../db/data.json"
+import './Home.css'
+
 const Home = ()=>{
 
     return(
         <div className="home">
-            Home
+            <h1>HOME</h1>
+            <div className="haedLine">
+                <h2>ANIMALS</h2>
+                <ContentsList props={dummy.animals} headline={true}/>
+            </div>
+            <div className="haedLine">
+                <h2>FOODS</h2>
+                <ContentsList props={dummy.foods} headline={true}/>
+            </div>
+            
         </div>
     )
 }
