f01eee504a1bbd31b2c69cc4f56f46d1da75ccf5
gydud25416 <152379377+gydud25416@users.noreply.github.com>
2024-05-08 15:47:30 +0900

    UPDATE:PUT
    UPDATE:PUT

--- reactjs/src/component/Word.js
+++ reactjs/src/component/Word.js
@@ -6,11 +6,27 @@ export default function Word({ it }){
     }
 
     function toggleDone(){ 
-        setIsDone(!isDone); 
+        fetch(`http://localhost:3001/words/${it.id}`,{
+            method:"PUT",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify({
+                ...it,
+                isDone:!isDone
+            })
+        })
+        .then(res=>{
+            if(res.ok){
+                setIsDone(!isDone); 
+            }
+        })
+        
     }
 
     const [isShow, setIsShow] = useState(false);
     const [isDone, setIsDone] = useState(it.isDone);
+    // const [word, setWord] = useState(it);
     return (
         <tr className={ isDone ? 'off' : ''}>
             <td><input type="checkbox" checked={ isDone} onChange={toggleDone}/></td>
