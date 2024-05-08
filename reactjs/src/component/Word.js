c19118fd3666d97401d06782eddf01e60e8203fa
gydud25416 <152379377+gydud25416@users.noreply.github.com>
2024-05-08 16:30:45 +0900

    Reactjs_DELETE:DELETE
    Reactjs_DELETE:DELETE

--- reactjs/src/component/Word.js
+++ reactjs/src/component/Word.js
@@ -1,6 +1,9 @@
 import {useState} from 'react'
 export default function Word({ it }){
-
+    const [del, setDel] = useState(it);
+    const [isShow, setIsShow] = useState(false);
+    const [isDone, setIsDone] = useState(it.isDone);
+    
     function toggleShow(){ 
         setIsShow(!isShow); 
     }
@@ -20,12 +23,26 @@ export default function Word({ it }){
             if(res.ok){
                 setIsDone(!isDone); 
             }
-        })
-        
+        }) 
     }
 
-    const [isShow, setIsShow] = useState(false);
-    const [isDone, setIsDone] = useState(it.isDone);
+    function onDelete(){
+        if(window.confirm("정말 삭제하시겠습니까?")){ 
+        fetch(`http://localhost:3001/words/${del.id}`,{
+            method:"DELETE"
+        })
+        .then(res=>{
+            if(res.ok){
+                setDel({id:0});
+            }
+        })
+        }
+    }
+    if(del.id === 0){
+        return null;
+    }
+
+
     // const [word, setWord] = useState(it);
     return (
         <tr className={ isDone ? 'off' : ''}>
@@ -34,7 +51,7 @@ export default function Word({ it }){
             <td>{isShow && it.kor}</td>
             <td>
                 <button onClick={toggleShow}>뜻 {isShow ? '숨기기' : '보기' }</button>
-                <button className="btn_del">삭제</button>
+                <button onClick={onDelete} className="btn_del">삭제</button>
             </td>
         </tr>
     )
