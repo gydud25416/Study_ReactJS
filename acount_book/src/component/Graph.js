86ccf10130e4c9d078c87cd86ee66fdcfc77d83d
gydud25416 <152379377+gydud25416@users.noreply.github.com>
2024-06-11 17:12:04 +0900

    [AcountBook] 그래프 스크립트 최적화
    [AcountBook] 그래프 스크립트 최적화

--- acount_book/src/component/Graph.js
+++ acount_book/src/component/Graph.js
@@ -6,37 +6,26 @@ import { useEffect, useState } from 'react';
 export default function Graph(){
     const graph = useFetch('http://localhost:3001/item');
     const [ plusFilter, setPlusFilter] = useState('+1');
-
-    const graph2024 = graph.filter((it)=>(it.year === "2024" && it.add === "+1")).reduce((sum, graph)=> sum + parseInt(graph.money, 10), 0);
-    const graph2023 = graph.filter((it)=>(it.year === "2023" && it.add === "+1")).reduce((sum, graph)=> sum + parseInt(graph.money, 10), 0); 
-    const graph2022 = graph.filter((it)=>(it.year === "2022" && it.add === "+1")).reduce((sum, graph)=> sum + parseInt(graph.money, 10), 0);
-    const graphTotal = graph.filter((it)=>(it.add === "+1")).reduce((sum, graph)=> sum + parseInt(graph.money, 10), 0);
-
-    const [toTal2024, SetToTal2024] = useState(parseInt((graph2024/graphTotal)*100));
-    const [toTal2023, SetToTal2023] = useState(parseInt((graph2023/graphTotal)*100));
-    const [toTal2022, SetToTal2022] = useState(parseInt((graph2022/graphTotal)*100));
- 
-    useEffect(()=>{
-        graphYear();
-    },[plusFilter, graph])
-
-    
+    const [yearFilter, setYearFilter] = useState({'2024':0, '2023':0, '2022':0});
+   
+    useEffect(()=>{ //초기 계산 함수 불러오기
+        graphYear(); 
+    },[plusFilter, graph]) 
     function PlusFilter(t){
         setPlusFilter(t);
-    }
-
-    function graphYear(){
-        const graph2024 = graph.filter((it)=>(it.year === "2024" && it.add === plusFilter)).reduce((sum, graph)=> sum + parseInt(graph.money, 10), 0);
-        const graph2023 = graph.filter((it)=>(it.year === "2023" && it.add === plusFilter)).reduce((sum, graph)=> sum + parseInt(graph.money, 10), 0); 
-        const graph2022 = graph.filter((it)=>(it.year === "2022" && it.add === plusFilter)).reduce((sum, graph)=> sum + parseInt(graph.money, 10), 0);
-
+    } 
+    function graphYear(){ //년도별 퍼센트 계산
         const graphTotal = graph.filter((it)=>(it.add === plusFilter)).reduce((sum, graph)=> sum + parseInt(graph.money, 10), 0);
-        SetToTal2024(parseInt((graph2024/graphTotal)*100))
-        SetToTal2023(parseInt((graph2023/graphTotal)*100))
-        SetToTal2022(parseInt((graph2022/graphTotal)*100)) 
-    }
 
-    
+        const years = [2024, 2023, 2022].map((year)=>(
+            graph.filter((it)=>(it.year === String(year) && it.add=== plusFilter )).reduce((sum, graph)=> sum + parseInt(graph.money, 10 ), 0) 
+        ))  
+        setYearFilter({
+            '2024': parseInt((years[0]/graphTotal)*100),
+            '2023': parseInt((years[1]/graphTotal)*100),
+            '2022': parseInt((years[2]/graphTotal)*100), 
+        }) 
+    } 
     return(
         <>
         <ul className='plusFilter graph'>
@@ -45,19 +34,12 @@ export default function Graph(){
                     <li className={plusFilter === "-1" ? "on" : ''} onClick={()=>PlusFilter("-1")}>지출</li>
                 </ul>
         <div className="wrap_graph">
-        <div className='graph_box'>
-                    <div className='graph_item ' style={{height: toTal2022 + "%"}}></div>
-                    <p>2022</p>
-            </div>
-            <div className='graph_box'>
-                    <div className='graph_item ' style={{height: toTal2023 + "%"}}></div>
-                    <p>2023</p>
-            </div>
-            <div className='graph_box'>
-                    <div className='graph_item'style={{height: toTal2024 + "%"}}></div>
-                    <p>2024</p> 
-            </div>
-
+            {[2022, 2023, 2024].map((year)=>(
+                <div className='graph_box' key={year}>
+                    <div className='graph_item ' style={{height: yearFilter[year] + "%"}}></div>
+                    <p>{year}</p>
+                 </div>
+            ))} 
         </div>
         </>
     )
