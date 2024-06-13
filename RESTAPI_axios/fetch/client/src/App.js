d3e4eafc9280b1f7d1c35db31bd89c18d8801938
gydud25416 <152379377+gydud25416@users.noreply.github.com>
2024-06-13 18:02:44 +0900

    [AXIOS] GET, POST
    [AXIOS] GET, POST

--- RESTAPI_axios/fetch/client/src/App.js
+++ RESTAPI_axios/fetch/client/src/App.js
@@ -1,27 +1,67 @@
 import { useEffect, useState } from "react";
-
+import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [todoList, setTodoList] = useState(null);
 
-fetch('http://localhost:4000/api/todo')
-.then(res=>{
-  return res.json();
-})
-.then(data=>{
-    setData(data);
-})
+  
+
+  const fetchData = async ()=>{
+    const res = await axios.get('http://localhost:4000/api/todo') 
+      setTodoList(res.data) 
+    // fetch('http://localhost:4000/api/todo')
+    // .then(res=>{
+    //   return res.json();
+    // })
+    // .then(data=>{
+    //   setTodoList(data);
+    // })
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+ 
+  function onSubmithandler(e){
+    e.preventDefault();
+    const text = e.target.text.value;
+    const done = e.target.done.checked;
+
+    axios.post('http://localhost:4000/api/todo', {text, done});
+    fetchData()
+    // fetch('http://localhost:4000/api/todo',{
+    //   method:"POST",
+    //   headers : {
+    //     'Content-Type' : 'application/json'
+    //   },
+    //   body : JSON.stringify({
+    //     text,
+    //     done,
+    //   })
+    // })
+    //  .then(()=>
+    //   fetchData()
+    // )
+  }
 
   return (
     <div className="App">
       <h1>TODO LIST</h1>
-      <ul>
-        {data.map((it)=>(
-          <li key={it.id}>
-              {it.text}
-          </li>
-        ))} 
-      </ul>
+      <form onSubmit={onSubmithandler}>
+        <input name="text"/>
+        <input name="done" type="checkbox"/>
+        <input name="submit" value={"추가"} type="submit" />
+      </form>
+       <ul>
+        {todoList?.map((todo)=>(
+          <div key={todo.id} style={{display:"flex"}}>
+            <div>{todo.id}</div>
+            <div>{todo.text}</div>
+            <div>{todo.done ? 'Y' : 'N'}</div>
+          </div>
+        ))}
+       </ul>
     </div>
   );
 }
--- RESTAPI_axios/fetch/server/app.js
+++ RESTAPI_axios/fetch/server/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cors());
-
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+ 
 let id = 2;
 const todoList = [{
     id:1,
